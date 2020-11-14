52968180549c4406a1b8b40e76302e34d914f156
saveroo <saveroo27@gmail.com>
2020-11-14 12:06:55 +0700

    chore(config): gulp and nuxt configuration

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,23 +1,46 @@
+// import bodyParser from 'body-parser'
+// import session from 'express-session'
+
 export default {
   // Target (https://go.nuxtjs.dev/config-target)
-  target: 'static',
+  target: 'server',
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: 'instadapp2',
+    title: 'Upwork',
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      {
+        name: 'description',
+        content:
+          'A fully featured admin theme which can be used to build CRM, CMS, etc.',
+      },
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1, shrink-to-fit=no',
+      },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    script: [
+      // Maps
+      { src: 'https://api.mapbox.com/mapbox-gl-js/v0.53.0/mapbox-gl.js' },
+    ],
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
-  css: [],
+  // TODO: Conditional Theme switcher
+  css: [
+    // '~/assets/css/theme.css',
+    '~/assets/fonts/feather/feather.css',
+    '~/assets/scss/theme.scss',
+    '~/assets/scss/theme-dark.scss',
+    '~/node_modules/flatpickr/dist/flatpickr.min.css',
+    '~/node_modules/quill/dist/quill.core.css',
+    '~/node_modules/highlightjs/styles/vs2015.css',
+  ],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-  plugins: [],
+  plugins: ['~/plugins/modes'],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
   components: true,
@@ -26,6 +49,8 @@ export default {
   buildModules: [
     // https://go.nuxtjs.dev/eslint
     '@nuxtjs/eslint-module',
+    '@nuxtjs/google-analytics',
+    '@nuxtjs/auth-next',
   ],
 
   // Modules (https://go.nuxtjs.dev/config-modules)
@@ -34,16 +59,62 @@ export default {
     '@nuxtjs/axios',
     // https://go.nuxtjs.dev/pwa
     '@nuxtjs/pwa',
+    '@nuxtjs/style-resources',
     // https://go.nuxtjs.dev/content
-    '@nuxt/content',
+    // '@nuxt/content',
   ],
 
-  // Axios module configuration (https://go.nuxtjs.dev/config-axios)
-  axios: {},
+  googleAnalytics: {
+    id: 'UA-156446909-1',
+  },
 
+  auth: {
+    localStorage: true,
+    strategies: {
+      local: {
+        tokenRequired: true,
+        tokenType: 'Bearer',
+        endpoints: {
+          login: {
+            url: '/auth/login',
+            method: 'post',
+            propertyName: 'token',
+          },
+          user: {
+            url: '/auth/user',
+            method: 'get',
+            propertyName: 'user',
+          },
+          logout: true,
+        },
+      },
+    },
+    redirect: {
+      logout: '/login',
+      login: '/login',
+      home: '/',
+    },
+    rewriteRedirects: true,
+  },
+
+  serverMiddleware: ['~/api/index.js'],
+
+  styleResources: {
+    scss: ['~/assets/scss/_user.scss', '~/assets/scss/_user-variables.scss'],
+  },
+
+  // Axios module configuration (https://go.nuxtjs.dev/config-axios)
+  axios: {
+    baseURL: 'http://localhost:3000/api',
+  },
+
+  // Causes 'toLowercase' error on SSR
   // Content module configuration (https://go.nuxtjs.dev/config-content)
-  content: {},
+  // content: {},
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
-  build: {},
+  build: {
+    // Don't deploy with this, this is for analyze purpose only
+    // analyze: true,
+  },
 }

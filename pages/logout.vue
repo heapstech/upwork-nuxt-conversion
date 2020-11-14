6ff0a7494252b2acbd6cef8879fedd073f654df7
saveroo <saveroo27@gmail.com>
2020-11-14 12:11:04 +0700

    feat(views): `Logout` asyncData $auth logout.

---
+++ pages/logout.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1 class="display-4 text-center mb-3">Logout</h1>
+    <div class="alert alert-info">
+      <span class="fe fe-alert-circle mr-4"></span>
+      Logging out
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  middleware: 'auth',
+  async asyncData(context) {
+    await context.$auth.logout()
+  },
+}
+</script>

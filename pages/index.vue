693974235c213df14e87067b25c86cdab0260df9
saveroo <saveroo27@gmail.com>
2020-11-14 12:11:19 +0700

    feat(views): [Account Page] conversion from legacy page with addition.

--- pages/index.vue
+++ pages/index.vue
@@ -1,63 +1,43 @@
 <template>
   <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">instadapp2</h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
+    <div class="row justify-content-center">
+      <div class="col-12 col-md-5 col-xl-4 my-5">
+        <h1 class="display-4 text-center mb-3">
+          {{ this.$auth.user.email }}
+        </h1>
+        <p class="text-muted text-center mb-5">Your account email</p>
+
+        <button
+          class="btn btn-sm btn-block btn-primary mb-1"
+          @click="toggleTheme()"
         >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+          Toggle Theme
+        </button>
+        <NuxtLink class="btn btn-sm btn-block btn-primary mb-1" to="logout">
+          Logout
+        </NuxtLink>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {}
+import { mapActions } from 'vuex'
+
+export default {
+  middleware: 'auth',
+  methods: {
+    ...mapActions({
+      toggleTheme: 'site/toggleTheme',
+    }),
+    async asyncData(context) {
+      await context.$auth.logout()
+    },
+  },
+  head() {
+    return {
+      title: 'Account',
+    }
+  },
+}
 </script>
-
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>

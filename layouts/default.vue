1b4e7892c66e7a8e073b6016230448f09b2cceae
saveroo <saveroo27@gmail.com>
2020-11-14 12:09:07 +0700

    feat(views): default layout conversion from legacy html.

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,55 +1,52 @@
 <template>
   <div>
-    <Nuxt />
+    <Nuxt class="" />
   </div>
 </template>
 
+<script>
+if (process.client) {
+  require('jquery/dist/jquery')
+  require('dropzone')
+  require('bootstrap')
+  require('@shopify/draggable')
+  require('autosize')
+  require('chart.js')
+  require('flatpickr')
+  require('highlightjs')
+  require('jquery-mask-plugin')
+  require('list.js')
+  require('quill')
+  require('select2')
+
+  require('~/assets/js/wizard')
+  require('~/assets/js/checklist')
+  // require('~/assets/js/tooltip')
+  require('~/assets/js/map')
+  require('~/assets/js/kanban')
+  require('~/assets/js/dashkit')
+}
+
+export default {
+  head() {
+    return {
+      link: [
+        {
+          rel: 'stylesheet',
+          href: `/css/${this.$store.getters['site/getColorScheme']}.css`,
+        },
+      ],
+      bodyAttrs: {
+        class:
+          'd-flex align-items-center bg-auth border-top border-top-2 border-primary',
+      },
+    }
+  },
+}
+</script>
+
 <style>
-html {
-  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
-    Roboto, 'Helvetica Neue', Arial, sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
-}
-
-*,
-*::before,
-*::after {
-  box-sizing: border-box;
-  margin: 0;
-}
-
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
+body {
+  display: none;
 }
 </style>
